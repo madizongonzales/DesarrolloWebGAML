f1ff93d3f759a0c0983d35f0aaf57968d5b1ee38
madizongonzales <madizonlgonzalesa@gmail.com>
2023-08-23 11:59:29 -0400

    Tarea3 Ejercicio1

--- Tareas/Tarea3/procesar.php
+++ Tareas/Tarea3/procesar.php
@@ -1,13 +1,8 @@
 <?php
-include ('funcion.php'); // Incluir el archivo con la función
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["cadena"])) {
-        $cadena = $_POST["cadena"];
-
+    include ('funcion.php');
+    if (isset($_GET["cadena"])) {
+        $cadena = $_GET["cadena"];
         $palabraMasLarga = PalabraMasLarga($cadena);
-
-        echo "La palabra más larga es: $palabraMasLarga";
+        echo "<p>La palabra más larga es: $palabraMasLarga</p>";
     }
-}
 ?>
