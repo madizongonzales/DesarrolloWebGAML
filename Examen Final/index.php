bb15e680ce56f338e4fec399d242b7f8def756af
madizongonzales <madizonlgonzalesa@gmail.com>
2023-12-02 00:14:27 -0400

    maqueta examen final

---
+++ Examen Final/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+<div class="arriba">
+    <div class="aiz">
+        <img src="img/logotecno.png" alt="logotecno" class="imagen">
+    </div>
+    <div class="ader">
+        <div id="menu">
+            <div class="button-1" onclick="pregunta1()">Pregunta 1</div>
+            <div class="button-1" onclick="pregunta2()">Pregunta 2</div>
+            <div class="button-1" onclick="pregunta3()">Pregunta 3</div>
+            <div class="button-1" onclick="pregunta4()">Pregunta 4</div>
+            <div class="button-1" onclick="pregunta5()">Pregunta 5</div>
+        </div>
+        <div id="titulo">
+            <div>Pregunta 1</div>
+           </div>
+    </div>
+</div>
+
+        <div class="contenido">
+            <div id="sub-menu">
+                <div class="opciones">Opciones</div>
+                <div class="detalle">Detalle 1</div>
+                <div class="detalle">Detalle 2</div>
+            </div>
+            <div id="contenido">
+                <div class="info">
+                    <div class="lila">
+                        <div><h3> SIS 256 Programacion Web</h3></div>
+                        <div> <h5>Examen Final - 02-12-2023 7:00 am</h5> </div>
+                    </div>
+                    <div class="fotografia">
+                        <img src="img/foto.jpg" alt="foto" class="foto">
+                    </div>
+                    <div >
+                        <div><h3>Gonzales Arancibia Madizon Lauren</h3></div>
+                        <div class="datos"><h5>Carrera: Ingeniería de Sistemas</h5></div>
+                        <div class="datos"><h5> Repostorio Examen: <a href="">madizonlgonzales</a></h5></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div id="pie">
+            Sucre - Semestre 2-2023
+        </div>
+    </div>
+</body>
+<script src="script.js"></script>
+
+</html>
